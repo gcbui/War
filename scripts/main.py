e25c3266e00f426e62688e20c8efd34bae2b039b
gcbui <you@example.com>
2022-05-28 15:21:17 -0700

    Started checking player win and playing a turn logic

--- scripts/main.py
+++ scripts/main.py
@@ -1,3 +1,4 @@
+from ctypes.wintypes import MAX_PATH
 from importlib.machinery import WindowsRegistryFinder
 from tkinter import N
 import random
@@ -14,6 +15,8 @@ class Card:
 
 global_deck = []
 players = []
+max_players = 4 
+current_players = []
 
 def init_deck():
     suits = ["HEARTS","DIAMONDS","SPADES","CLUBS"]    
@@ -27,12 +30,16 @@ def init_deck():
 
 def shuffle_deck():
     temp_deck = global_deck
+    length_of_temp_deck = len(temp_deck)
     shuffled_deck = []
-    for i in range(52):
-        random_select = random.randint(0,len(temp_deck))
+    #for i in range(len(temp_deck)):
+        #print (str(temp_deck[i].number) + temp_deck[i].suit)
+    for i in range(length_of_temp_deck):
+        random_select = random.randint(0,len(temp_deck)-1)
         shuffled_deck.append(temp_deck[random_select])
+        #print(str(shuffled_deck[len(shuffled_deck)-1].number))
         del temp_deck[random_select]
-    return shuffled_deck
+    print(len(shuffled_deck))
 
 def create_players():
     wrong_selection = "default"
@@ -47,7 +54,7 @@ def create_players():
             wrong_selection = "default"
         elif len(wrong_selection) >= 1:
             num_of_players = int(wrong_selection[0])
-            if 1 < num_of_players and num_of_players < 5:  
+            if 1 < num_of_players and num_of_players < (max_players+1):  
                 wrong_selection = "correct"  
                 for num in range(num_of_players):
                     player = Player()
@@ -58,6 +65,7 @@ def create_players():
         else:
             print("Select valid number of players. Retry! ")
             wrong_selection = "default"
+    current_players = players
     return players    
 
 
@@ -66,9 +74,37 @@ def split_deck(players,deck):
         players[num % len(players)].cards.append(deck[num])
     return players
 
-def player_that_wins(players):
-    pass
-    #.pop(0)
+#players = [player object with a .number and .cards ,]
+def play_a_turn(players):
+    print ("Player # "+player.number+"'s card is equal to "+player.cards[0].number+" of "+player.cards[0].suit)
 
+def did_player_win(players): #return player that won and returns True or False if player los
+
+def player_that_wins(players):
+    did_player_win = False
+    while did_player_win == False:
+        for i in range(len(players)):
+            if len(players[i].cards) == 0:    #remove player because they lost
+                players.remove(players[i])       
+            elif len(players[i].cards) == 52:
+                print("PLAYER NUMBER " +players[i].number + " WINS!")
+                did_player_win == True
+            else:
+                show_hand(players[i])
+        
+
+
+
+        while len(players[i].cards) != 52:
+            
+                current_players = 
+        
+    
+    max_card = 0
+    
+    for player in players:
+        if player.card.number > max_card:
+            max_card = player.card.number
+            winner = player
     
 
